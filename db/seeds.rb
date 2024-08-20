f5576feafdb0dc14bf4d27c1fb982766e90c9e2e
patrick-204 <p.ohara921@gmail.com>
2024-08-19 23:24:02 -0400

    Create a basic CRUD (Create, Read, Update, Delete) application to manage a list of programming languages.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Clear existing records to prevent duplication
+ProgrammingLanguage.destroy_all
+
+# Create sample programming languages
+ProgrammingLanguage.create!(
+  title: 'Ruby',
+  description: 'A dynamic, open source programming language with a focus on simplicity and productivity.',
+  link: 'https://www.ruby-lang.org/en/'
+)
+
+ProgrammingLanguage.create!(
+  title: 'Python',
+  description: 'A programming language that lets you work quickly and integrate systems more effectively.',
+  link: 'https://www.python.org/'
+)
+
+ProgrammingLanguage.create!(
+  title: 'JavaScript',
+  description: 'A high-level, often just-in-time compiled, and multi-paradigm programming language.',
+  link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+)
+
+ProgrammingLanguage.create!(
+  title: 'Java',
+  description: 'A high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.',
+  link: 'https://www.java.com/en/'
+)
+
+ProgrammingLanguage.create!(
+  title: 'C++',
+  description: 'A high-performance language that is widely used in software development, including system/software development, game development, and real-time simulations.',
+  link: 'https://isocpp.org/'
+)
+
+ProgrammingLanguage.create!(
+  title: 'Rust',
+  description: 'A systems programming language focused on safety, speed, and concurrency.',
+  link: 'https://www.rust-lang.org/'
+)
